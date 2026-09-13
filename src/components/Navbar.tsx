a1e0caf828410e1a448abbe49842c5ae4145fd2e
unknown <Limon>
2026-09-13 11:21:54 +0600

    create the navar logo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from "../assets/logo-text.png"
+const Navbar = () => {
+    return (
+      <header className="sticky top-0 z-50 bg-white/80 backdrop-blur-md border-b border-gray-100">
+        <div className="container mx-auto px-4 md:px-8 h-16 flex items-center justify-between">
+          {/* ভেতরের উপাদানগুলো এখানে বসবে */}
+
+          <a
+            href="#"
+            className="flex items-center gap-2 hover:opacity-90 transition"
+          >
+            <img
+              src={Logo}
+              alt="Dev Stack Logo"
+              className="h-7 w-auto object-contain"
+            />
+          </a>
+        </div>
+      </header>
+    );
+};
+
+export default Navbar;
